5af6d86ea9f01a3551bd8291e26f803e31975bda
fabricio-leitao <fabricio-beco@hotmail.com>
2021-02-14 09:51:52 -0300

    Projeto criado, classe autor criada

---
+++ src/main/java/br/com/zup/casa/codigo/model/Autor.java
@@ -0,0 +1,12 @@
+package br.com.zup.casa.codigo.model;
+
+import java.time.Instant;
+
+public class Autor {
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String descricao;
+	private Instant dataCriacao;
+}
